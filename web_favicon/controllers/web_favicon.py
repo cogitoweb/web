d25a21e08e95c7d5c5d08fc63c26b076ff52d056
Davide Viotto <davide.viotto@cogitoweb.it>
2025-03-25 14:57:50 +0100

    Fix encoding

--- web_favicon/controllers/web_favicon.py
+++ web_favicon/controllers/web_favicon.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
-# © 2015 Therp BV <http://therp.nl>
+# Copyright 2015 Therp BV <http://therp.nl>
+# Copyright 2017 QubiQ 2010 <http://www.qubiq.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import io as StringIO
+from io import BytesIO
 import base64
 from odoo import http
 from odoo.tools.misc import file_open
@@ -21,9 +21,9 @@ class WebFavicon(http.Controller):
         favicon = company.favicon_backend
         favicon_mimetype = company.favicon_backend_mimetype
         if not favicon:
-            favicon = file_open('web/static/src/img/favicon.ico')
+            favicon = file_open('web/static/src/img/favicon.ico', 'rb')
             favicon_mimetype = 'image/x-icon'
         else:
-            favicon = StringIO.StringIO(base64.b64decode(favicon))
+            favicon = BytesIO(base64.b64decode(favicon))
         return request.make_response(
             favicon.read(), [('Content-Type', favicon_mimetype)])
